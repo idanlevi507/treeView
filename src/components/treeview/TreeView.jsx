bb3793dd7ffe96e8efb0814203770cd952f67ef0
Tom Slutsky <tom.slutsky@gmail.com>
2022-01-27 10:57:09 +0200

    init

---
+++ src/components/treeview/TreeView.jsx
@@ -0,0 +1,14 @@
+import './treeview.css';
+
+
+let TreeView = (props) => {
+
+    let {data} = props;
+
+
+    return <div className='treeview-root'>
+        <p>Here you should implement the component</p>
+    </div>
+}
+
+export default TreeView;
